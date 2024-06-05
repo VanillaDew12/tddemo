ba1857a3abce344593dc39f2111106f05b0fec71
VanillaDew12 <huangyuzhe2000@126.com>
2024-06-05 16:20:18 +0800

    第一次尝试

---
+++ src/main/java/com/example/tddemo/TddemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.tddemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TddemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TddemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/tddemo/TddemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tddemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TddemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
